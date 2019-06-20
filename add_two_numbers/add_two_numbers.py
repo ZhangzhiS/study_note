bd2f6a73c47216a3fb53e9e975223594043aa15a
zhangzhi <zhangzhi@botpy.com>
2019-06-20 14:22:45 +0800

    两数相加
    规范注释文本

--- add_two_numbers/add_two_numbers.py
+++ add_two_numbers/add_two_numbers.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
 """
 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
 
@@ -29,6 +32,11 @@ def add_two_nums(l1: ListNode, l2: ListNode) -> ListNode:
 
 
 def list_node_to_nums(list_node: ListNode) -> int:
+    """
+    将ListNode转换为整数
+    :param list_node: ListNode
+    :return: int
+    """
     num_list = []
     temp_node = list_node
     while True:
@@ -44,12 +52,16 @@ def list_node_to_nums(list_node: ListNode) -> int:
 
 
 def nums_to_list_node(nums: int) -> ListNode:
+    """
+    将数字转换为ListNode
+    :param nums: 整数
+    :return: ListNode
+    """
     nums_list = list(str(nums))
-    # print(nums_list)
     last = True
+    new_node = None
     temp_node = None
     for i in nums_list:
-        # print(i)
         if last is True:
             new_node = ListNode(i)
             last = False
